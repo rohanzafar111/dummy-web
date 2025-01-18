565d15f83f03fa6dde83b4f08b6b11018a1cadac
rohanzafar111 <rohanzafar419@gmail.com>
2025-01-18 07:31:03 -0800

    Add files via upload

---
+++ haider_himself/assets/script.js
@@ -0,0 +1,17 @@
+/* let temp = prompt(parseInt('What is your current Temperature'));
+
+  if(temp <= 10){
+document.write("the current Temperature is Cold")
+  }else if(temp > 10 && temp <= 35 ){
+document.write("the current Temperature is Moderate")
+  }else if(temp > 35 && temp <= 50){
+    document.write("the current Temperature is Hot")
+  }else if(temp > 50 && temp <= 100){
+    document.write("You are currently in Hell")
+  }  */
+
+let x = "javascript is amazing";
+
+for (let i=0; i<x.lenght; i++){
+    console.log(i)
+}
